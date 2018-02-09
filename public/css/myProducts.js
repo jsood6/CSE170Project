44916eb59f47ddca0c8f5814db855663d6aec4af
Jigyasaa Sood <jsood@ucsd.edu>
2018-02-08 20:46:16 -0800

    need to fix some home screen layout and fix html link for myProducts in the drop down menu

---
+++ public/css/myProducts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+	/*$('.friend a').click(function(e){
+		e.preventDefault();
+		$(this).find('h3').text(anagrammedName($(this).find('h3').text()));
+	});*/
+})
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	console.log("Javascript connected!");
+}
+
