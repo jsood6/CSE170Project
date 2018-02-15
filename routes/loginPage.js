5edd5bf8c4bcb15c294d4499ade9f83e51cfdff4
Jigyasaa Sood <jsood@ucsd.edu>
2018-02-15 00:35:41 -0800

    google sign in works, fixing design

--- routes/loginPage.js
+++ routes/loginPage.js
@@ -1,4 +1,20 @@
+/*var data = require("../data.json");
 
+exports.addFriend = function(req, res) {    
+	// Your code goes here
+	var newFriend = {
+		"name" : req.query.name,
+		"description" : req.query.description,
+		"imageURL" : 'http://lorempixel.com/400/400/people'
+
+	};
+
+	console.log();
+	data.friends.push(newFriend);
+
+	res.render('index', data);
+
+ }; */
 /*
  * GET home page.
  */
