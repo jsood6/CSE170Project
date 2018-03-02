7694631f433bd6e674c61fcc2d1f40e40f5c2a1b
Jigyasaa Sood <jsood@ucsd.edu>
2018-03-01 22:35:23 -0800

    added help button on login page

--- public/js/loginPage.js
+++ public/js/loginPage.js
@@ -91,6 +91,32 @@ function isUserEqual(googleUser, firebaseUser) {
   return false;
 }
 
+// Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
 
 /*function onSignIn(googleUser) {
         // Useful data for your client-side scripts:
